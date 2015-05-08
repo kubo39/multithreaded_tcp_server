e4a28d326d435cfa6b1159e44001cbbf3ae4789a
kubo39 <kubo39@gmail.com>
2015-05-09 06:26:52 +0900

    Initial commit

---
+++ tcpserver.d
@@ -0,0 +1,80 @@
+import std.stdio;
+import std.socket;
+import std.concurrency;
+
+
+class TcpListener
+{
+  shared TcpSocket listener;
+
+  this(ushort port)
+  {
+    TcpSocket tmp = new TcpSocket;
+    assert(tmp.isAlive);
+    tmp.setOption(SocketOptionLevel.SOCKET, SocketOption.REUSEADDR, 1);
+    tmp.bind(new InternetAddress(port));
+    listener = cast(shared) tmp;
+  }
+
+  void listen(int backlog)
+  {
+    (cast()listener).listen(backlog);
+  }
+
+  Socket accept() shared
+  {
+    Socket sock = (cast()listener).accept;
+    scope (failure) {
+      if (sock !is null) {
+        sock.close;
+      }
+    }
+    return sock;
+  }
+}
+
+
+class ThreadedServer
+{
+  TcpListener listener;
+
+  this(ushort port, int backlog = 1024)
+  {
+    listener = new TcpListener(port);
+    listener.listen(backlog);
+  }
+
+  void run(void function(Socket) handler, uint nthreads = 2)
+  {
+    for (int i; i< nthreads; ++i) {
+      spawn(&spawnedFunc, thisTid, cast(shared)listener, handler);
+    }
+    for (;;) { /* sleep */ };
+  }
+}
+
+
+void spawnedFunc(Tid ownerTid, shared(TcpListener) listener,
+                 void function(Socket) handler)
+{
+  for (;;) {
+    Socket sock = listener.accept;
+    writeln("Accepted: ", thisTid);
+    handler(sock);
+  }
+}
+
+
+void main()
+{
+  auto server = new ThreadedServer(4000);
+  server.run((sock) {
+      ubyte[1024] buffer;
+      scope (exit) sock.close;
+
+      sock.receive(buffer);
+      writeln(cast(string) buffer);
+      sock.send(buffer);
+    });
+}
+
