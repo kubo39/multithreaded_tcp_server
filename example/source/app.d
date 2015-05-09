9a36b46af6d332fc36d4ca0506339ef9b5353f67
kubo39 <kubo39@gmail.com>
2015-05-09 21:02:55 +0900

    Add example echoserver

---
+++ example/source/app.d
@@ -0,0 +1,17 @@
+import threadedtcpserver;
+
+import std.stdio;
+
+
+void main()
+{
+  auto server = new ThreadedTcpServer(4000);
+  server.run((sock) {
+      ubyte[1024] buffer;
+      scope (exit) sock.close;
+
+      sock.receive(buffer);
+      writeln(cast(string) buffer);
+      sock.send(buffer);
+    });
+}
