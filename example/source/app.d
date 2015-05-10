bcf5d7f62d424d6e5f31e8596cdf02c302855197
kubo39 <kubo39@gmail.com>
2015-05-11 06:09:38 +0900

    Update example echo server

--- example/source/app.d
+++ example/source/app.d
@@ -5,11 +5,10 @@ import std.stdio;
 
 void main()
 {
-  auto server = new ThreadedTcpServer(4000);
-  server.listen;
+  auto server = new ThreadedTcpServer("localhost", 4000);
+  server.listen(1024);
   server.run((sock) {
       ubyte[1024] buffer;
-      scope (exit) sock.close;
 
       sock.receive(buffer);
       writeln(cast(string) buffer);
