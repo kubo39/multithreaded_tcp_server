fe433428d6aabc37a19a00da379f0bb33864084b
Hiroki Noda <kubo39@gmail.com>
2017-03-26 21:10:20 +0900

    Up

--- example/source/app.d
+++ example/source/app.d
@@ -1,17 +1,16 @@
 import threadedtcpserver;
-
 import std.stdio;
-
 
 void main()
 {
-  auto server = new ThreadedTcpServer("localhost", 4000);
-  server.listen(1024);
-  server.run((sock) {
-      ubyte[1024] buffer;
+    stderr.writeln("Listening localhost:4000.");
+    auto server = new ThreadedTcpServer("localhost", 4000);
+    server.listen(1024);
+    server.run((sock) {
+            ubyte[1024] buffer;
 
-      sock.receive(buffer);
-      writeln(cast(string) buffer);
-      sock.send(buffer);
-    });
+            sock.receive(buffer);
+            writeln(cast(string) buffer);
+            sock.send(buffer);
+        });
 }
